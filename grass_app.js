4da9fce49c4f9718b72710026cd428f5c0b65c4c
Ubuntu <ubuntu@ip-172-31-47-209.ec2.internal>
2020-10-08 01:47:12 +0000

    Added the userId functionality to productDetail page

--- grass_app.js
+++ grass_app.js
@@ -106,6 +106,7 @@ app.post('/login', async function(req, res){
     if(passwordMatch){
         req.session.authenticated = true;
         req.session.user = isUserExist[0].username;
+        req.session.userId = isUserExist[0].user_id;
         res.redirect('/loginHome');
     }
     else{
@@ -189,7 +190,7 @@ app.get('/productDetail/:id/:productId', function(req, res) {
                 else{
                     console.log(result2);
                     if(req.session.authenticated){
-                        res.render('productDetails', {authenticated: true, username: req.session.user, id: req.session.id, grassSpecific: result1[0], grassGeneral: result2[0]}); // need a productDetails view
+                        res.render('productDetails', {authenticated: true, username: req.session.user, id: req.session.userId, grassSpecific: result1[0], grassGeneral: result2[0]}); // need a productDetails view
                     }
                     else{
                         res.render('productDetails', {authenticated: false, username: "", id: -1, grassSpecific: result1[0], grassGeneral: result2[0]});
